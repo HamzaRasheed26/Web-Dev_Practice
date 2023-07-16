d48b3586c9f90485169c2ee35edf7f0bf2dca884
HamzaRasheed26 <hamzarasheed19961@gmail.com>
2023-07-16 22:54:14 +0500

    design

--- React/color_picker/src/Components/form.jsx
+++ React/color_picker/src/Components/form.jsx
@@ -8,9 +8,22 @@ export default function Form() {
     };
 
   return (
-    <div style={{backgroundColor: `${color}`}}>
-      <h1>Hamza</h1>
-      <button onClick={ChangeColor}>Click me</button>
+    <div >
+      <h1>Color Picker</h1>
+      <div className='form' style={{backgroundColor: `${color}`}}>
+        <h2>Form</h2>
+        <div className='name'>
+            <input type="text" placeholder='First name' className='first'/>
+            <input type="text" placeholder='Last name'className='last'/>
+        </div>
+        <br />
+        <input type="text" placeholder='Address' className='address'/> <br />
+        <input type="text" placeholder='Email' className='email'/> <br />
+        <input type="text" placeholder='Phone' className='phone'/> <br />
+        <input type="color" name="" className="colorPicker" placeholder='Color Picker'/> <br />
+        <button className='submit' onClick={ChangeColor}>Change Color</button>
+      </div>
+      
     </div>
   )
 }
