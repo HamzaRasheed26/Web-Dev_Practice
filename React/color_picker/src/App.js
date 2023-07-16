700c404d6e2c029e10945a9a0ec5e612d4e7579b
HamzaRasheed26 <hamzarasheed19961@gmail.com>
2023-07-16 21:54:20 +0500

    Learning React Start

---
+++ React/color_picker/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Switch></Switch>
+    </div>
+  );
+}
+
+export default App;
